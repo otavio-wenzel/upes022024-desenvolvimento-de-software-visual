efc691124ce6961d86a1c34f497b4ac5e8eaacc3
otavio-wenzel <otavio.wenzel@gmail.com>
2024-09-24 19:44:09 -0300

    Commit para verificação

--- SistemaDeGerenciamentoEscolar/Program.cs
+++ SistemaDeGerenciamentoEscolar/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+public class BancoDeDados : DbContext {
 
-app.Run();
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder builder)
+    {
+        string credencial = "server=localhost;port=3306;database=planner;user=root;password=positivo";
+
+        builder.UseMySQL(credencial);
+        //registros da solictação da api
+        //.LogTo(Console.WriteLine, LogLevel.Information);
+        
+    }
+
+    public DbSet<Aluno> Alunos => Set<Aluno>();
+    public DbSet<Professor> Professores => Set<Professor>();
+    public DbSet<Disciplina> Disciplinas => Set<Disciplina>();
+    public DbSet<Turma> Turmas => Set<Turma>();
+
+}
