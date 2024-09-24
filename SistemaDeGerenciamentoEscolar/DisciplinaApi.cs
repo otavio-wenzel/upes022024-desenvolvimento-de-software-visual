e1b3b25980e8318c7517668c855f4e5c02b82227
lucascoelhofelix <167718769+lucascoelhofelix@users.noreply.github.com>
2024-09-24 16:34:10 -0300

    Update DisciplinaApi.cs
    criação de codigo

---
+++ SistemaDeGerenciamentoEscolar/DisciplinaApi.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+
+public static class DisciplinaApi
+{
+  public static void MapDisciplinaApi(this WebApplication app)
+  {
+    var tabela = app.MapGroup("/disciplinas");
+
+    tabela.MapGet("/discilinas", async (BancoDeDados db) =>
+    await db.Disciplinas.ToListAsync());
+
+    tabela.MapGet("/disciplinas/{id}", async (int id, BancoDeDados db) => 
+        await db.Dsciplinasos.FindAsync(id)
+        is Disciplina disciplina
+            ? Results.Ok(disciplina)
+            : Results.NotFound());
+
+    tabela.MapPost("/disciplinas", async (Disciplina disciplina, BancoDeDados db) => {
+        db.Disciplinas.Add(disciplina);
+        await db.SaveChangesAsync();
+        return Results.Created($"/disciplinas/{disciplina.Id}", disciplina);
+    });
+
+    tabela.MapPut("disciplina/{id}", async (int id, Disciplina disciplinaAtualizada, BancoDeDados db) =>
+    {
+        var disciplina = await db.Disciplinas.FindAsync(id);
+        if (disciplina is null) return Results.NotFound();
+
+        disciplina.Materia = alunoAtualizado.Materia;
+
+        await db.SaveChangesAsync();
+
+        return Results.NoContent();
+    });
+
+    tabela.MapDelete("disciplinass/{id}", async (int id, BancoDeDados db) =>
+    {
+        if(await db.Disciplinass.FindAsync(id) is Disciplina disciplina){
+
+            db.Disciplinas.Remove(disciplina);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        }
+        return Results.NotFound();
+    
+    });
+  }
+}
